0db6ccae933b5ff3137f53c55068e5c7a8af4e5e
David Zim <davidzim29.90@gmail.com>
2023-01-10 23:41:35 +0200

    fix product p overflow-wrap and add some footer content

--- src/components/layout/footer/footer.js
+++ src/components/layout/footer/footer.js
@@ -3,7 +3,15 @@ import './footerStyle.css';
 function Footer() {
     return (
         <div className="footer container">
-            <p>footer&copy;David Zim</p>
+            <p>
+                This project has made during school time to learn and practice web development skills
+            <br/>
+                &copy;David Zim
+                <br/>
+                052-325-3813
+                <br/>
+                davidzim29.90@gmail.com
+            </p>
         </div>
     );
 }
